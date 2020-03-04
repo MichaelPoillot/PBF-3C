39624b0aab4b20eac4815f245eb8977e5ee04de3
MichaelPoillot <michaelkristyan@gmail.com>
2020-03-04 19:05:11 +0700

    mantappp

---
+++ Modul 3/src/container/StatefullComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StatefullComponent extends React.Component {
+    render() {
+        return <p>ini adalah state </p>
+    }
+}
+
+export default StatefullComponent;
